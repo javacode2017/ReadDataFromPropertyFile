48ae29cbb4fcc48b23e60afbeb9abac65855ebac
sujith <sujithreddy440@gmail.com>
2020-03-13 22:02:07 +0530

    initial commit of read data from properties file

---
+++ src/main/java/com/example/readdaat/ReadDataFromPropertyFile/ReadDataFromPropertyFileApplication.java
@@ -0,0 +1,13 @@
+package com.example.readdaat.ReadDataFromPropertyFile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadDataFromPropertyFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadDataFromPropertyFileApplication.class, args);
+	}
+
+}
