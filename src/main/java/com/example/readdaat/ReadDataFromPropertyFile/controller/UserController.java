5348c8394ad78a178f4ae753b57e9df3cbbe53b2
sujith <sujithreddy440@gmail.com>
2020-03-14 12:18:04 +0530

    enable logging with File configuration

--- src/main/java/com/example/readdaat/ReadDataFromPropertyFile/controller/UserController.java
+++ src/main/java/com/example/readdaat/ReadDataFromPropertyFile/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.example.readdaat.ReadDataFromPropertyFile.controller;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.readdaat.ReadDataFromPropertyFile.model.User;
+
+import ch.qos.logback.classic.Logger;
 
 @RestController
 public class UserController {
@@ -12,9 +15,22 @@ public class UserController {
 	@Autowired
 	User user;
 	
+	
+	Logger log = (Logger) LoggerFactory.getLogger(UserController.class);
+
 	@GetMapping("/user")
 	public String UserDetails() {
 		System.out.println(user.getUid()+""+user.getUname());
 		return user.toString();
+	}
+	
+	@GetMapping("/logs")
+	public String  logs() {
+		log.trace("trace");
+		log.debug("debug");
+		log.info("info");
+		log.warn("warn");
+		log.error("error");
+		return "logs demo";
 	}
 }
